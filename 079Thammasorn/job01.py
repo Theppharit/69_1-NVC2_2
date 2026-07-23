a0908c3f2830a58bfc5f5fb293a6f21b1dd1e08b
gun1147 <sasikanpanklang0908@gmail.com>
2026-07-23 16:08:07 +0700

    ko

---
+++ 079Thammasorn/job01.py
@@ -0,0 +1,6 @@
+name = "นาย"
+print("Greetings my names" + name )
+print(name + "im in the 11th grade")
+print(name + "my student id is 68219100079")
+print(name + "and studying at nakhon ratchasima vocational collage")
+print(name + "my major is digital business technology")
