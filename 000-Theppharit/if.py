625aae8b3b02830390fbf5dc396c140ec5eb95ba
Th.To. <96429296+Theppharit@users.noreply.github.com>
2026-08-13 13:55:32 +0700

    +

---
+++ 000-Theppharit/if.py
@@ -0,0 +1,10 @@
+price = 198
+
+if price >= 899 :
+    print("เมนูพรีเมียม")
+elif price >= 499 :
+    print("เมนูชั้นดี")
+elif price >= 199 :
+    print("เมนูทั่วไป")
+else :
+    print("กรุณากลับมาใหม่ภายหลัง")
