30836b65954d34419a008cfc2066c2122fcfe231
Th.To. <96429296+Theppharit@users.noreply.github.com>
2026-08-27 13:59:12 +0700

    +

---
+++ 000-Theppharit/05-list.py
@@ -0,0 +1,15 @@
+name1 = "b1"
+name2 = "b2"
+name3 = "b3"
+name4 = "b4"
+
+class2_1 = [name1, name2, name3, name4]
+
+class2_1[2] = "no name"
+
+print(class2_1)
+print(class2_1[3])
+
+number = "9876543210"
+
+print(number[5])
