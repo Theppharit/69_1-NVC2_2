86972eccdc6ca95b3856e7f7e996d8ab56369b42
Th.To. <96429296+Theppharit@users.noreply.github.com>
2026-08-27 14:19:13 +0700

    +

--- 000-Theppharit/05-list.py
+++ 000-Theppharit/05-list.py
@@ -6,11 +6,13 @@ name5 = "b5"
 
 class2_2 = [name1, name2, name3, name4, name5]
 
-# class2_2[2] = "no name"
 
-# print(class2_2)
-# print(class2_2[3])
+class2_2[2] = "SS"
 
-# number = "9876543210"
 
-# print(number[5])
+print(class2_2)
+print(class2_2[0])
+
+number = "9876543210"
+
+print(number[7])
