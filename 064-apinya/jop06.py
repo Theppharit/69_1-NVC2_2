9ba96365aba268ce5a3a61af1bc4027e8f8aa691
suer0660851120-a11y <zxapinya@gmail.com>
2026-09-10 14:02:21 +0700

    ++

--- 064-apinya/jop06.py
+++ 064-apinya/jop06.py
@@ -1,13 +1,17 @@
+# Control Structures in Python
 
+# Loops in Python
+# การใช้ลูป for และ while ใน Python
 
+# count = 0
 
-
-
-
+# while count < 5:
+#     print("Count is:", count)
+#     count += 1
 
 
 number = 0
 
-while number <= 12:
-    print("number is:", number * 2)
-    number +1
+while number <=12:
+    print("Number is:", number * 2)
+    number += 1
