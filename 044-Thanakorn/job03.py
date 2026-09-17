fbfb4f64f31e28e30a867ecf29e9208635d654c8
prarewa2550-prog <102844648+kawjaw12@users.noreply.github.com>
2026-09-17 12:30:24 +0700

    +
    +

---
+++ 044-Thanakorn/job03.py
@@ -0,0 +1,11 @@
+price =int(input("กรอกคะแนน :"))
+if price >= 80 :
+    print("A")
+elif price >= 70 :
+    print("B")
+elif price >= 60 :
+    print("C")
+elif price >= 50 :
+    print("D")
+else :
+    print("F")
